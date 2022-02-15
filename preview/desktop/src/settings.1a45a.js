9555f7b3186d9aa91670e34ff1118bfd491a405a
henianer <1097119262@qq.com>
2022-02-15 14:17:22 +0800

    update

--- preview/desktop/src/settings.1a45a.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/resources/scenes/AStar.fire",orientation:"",jsList:[],bundleVers:{internal:"1b082",resources:"dcc4f",main:"fdf58"}};
